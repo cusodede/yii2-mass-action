a2e32b7ca37fb174f796c28772323912cb3b7a5d
Pozitronik <antikillerxxl@gmail.com>
2023-03-13 12:37:00 +0400

    users fixture

---
+++ tests/_app/fixtures/data/users.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+return [
+	'admin' => [
+		'id' => 1,
+		'login' => 'admin',
+		'password' => 'admin',
+		'username' => 'Системный администратор',
+	],
+	'manager' => [
+		'id' => 2,
+		'login' => 'manager',
+		'password' => 'manager',
+		'username' => 'Менеджер Билайн',
+	],
+	'some' => [
+		'id' => 3,
+		'login' => 'some',
+		'password' => 'some',
+		'username' => 'some user',
+	],
+	'user1' => [
+		'id' => 4,
+		'login' => 'user1',
+		'password' => 'user1',
+		'username' => 'user1 user',
+	],
+	'user2' => [
+		'id' => 5,
+		'login' => 'user2',
+		'password' => 'user2',
+		'username' => 'user2 user',
+	],
+	'user3' => [
+		'id' => 6,
+		'login' => 'user3',
+		'password' => 'user3',
+		'username' => 'user3 user',
+	],
+	'user4' => [
+		'id' => 8,
+		'login' => 'user4',
+		'password' => 'user4',
+		'username' => 'user4 user',
+	],
+	'user5' => [
+		'id' => 9,
+		'login' => 'user5',
+		'password' => 'user5',
+		'username' => 'user5 user',
+	],
+	'user6' => [
+		'id' => 10,
+		'login' => 'user6',
+		'password' => 'user6',
+		'username' => 'user6 user',
+	],
+	'user7' => [
+		'id' => 11,
+		'login' => 'user7',
+		'password' => 'user7',
+		'username' => 'user7 user',
+	],
+];
